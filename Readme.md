ff8747036d051bc34062fc9e78c895ed4d30579f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to determine if the ASPxGridView is in the insert mode at the client side
+
+
+<p>The example demonstrates how to determine if ASPxGridView is in the new row editing mode on the client side. The <strong>CustomJSProperties</strong> event is used to pass the required information from the server side to the client side.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub ASPxGridView1_CustomJSProperties(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewClientJSPropertiesEventArgs)
+		e.Properties("cpIsNewRowEditing") = (TryCast(sender, ASPxGridView)).IsNewRowEditing.ToString()
+	End Sub
+End Class
